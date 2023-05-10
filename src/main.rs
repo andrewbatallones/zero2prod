3836963dee8ae0b2e1fc2ec3a82d34d309286ca1
Andrew Batallones <batallonesa@gmail.com>
2023-05-10 13:53:04 -0400

    add actix and health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpResponse, HttpServer, App, web};
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().body("Status OK")
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[actix_web::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+
+        assert!(response.status().is_success())
+    }
 }
