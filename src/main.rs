83b1a2797962cc6f6679771a0497986a44baa35c
Andrew Batallones <batallonesa@gmail.com>
2023-05-10 14:27:40 -0400

    add tokio and move logic

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,6 @@
-use actix_web::{HttpResponse, HttpServer, App, web};
+use zero2prod::run;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().body("Status OK")
-}
-
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::health_check;
-
-    #[actix_web::test]
-    async fn health_check_succeeds() {
-        let response = health_check().await;
-
-        assert!(response.status().is_success())
-    }
+    run().await
 }
