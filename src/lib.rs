385589ade81eab36f0191f43526e8cdb20d8f0d3
Andrew Batallones <batallonesa@gmail.com>
2023-05-22 11:34:04 -0400

    modularlize lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,13 @@
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
 
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+mod configuration;
+mod routes;
 
 pub fn run(port: u16) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/subscriptions", web::post().to(subscribe))
-            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(routes::subscriptions::subscribe))
+            .route("/health_check", web::get().to(routes::health_check::health_check))
     })
     .bind(("127.0.0.1", port))?
     .run();
