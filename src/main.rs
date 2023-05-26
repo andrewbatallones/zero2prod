8bb39d9d518315ea120c098bb9c0a0ebb509e7c6
Andrew Batallones <batallonesa@gmail.com>
2023-05-26 11:28:32 -0400

    add config logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use zero2prod::startup::run;
+use zero2prod::{startup::run, configuration::get_configuration};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run(8080)?.await
+    let configuration = get_configuration().expect("failed to read configuration.");    
+    let port = configuration.application_port;
+
+    run(port)?.await
 }
