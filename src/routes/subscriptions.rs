ba0df5fd76bbfffa57fc1c0528bf3ef83cc769b8
andrewbatallones <batallonesa@gmail.com>
2024-05-03 14:57:43 -0400

    replace log with tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,14 +12,21 @@ pub struct FormData {
 /// POST /subscribe
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    let request_id_log = format!("request_id {} - ", request_id);
 
-    tracing::info!(
-        "{}Adding '{}' '{}' as a new subscriber.",
-        request_id_log,
-        form.email,
-        form.name
+    // Span also have their own levels
+    // This creates a span at the info-level
+    // `%` using that symbol is telling tracing to use their Display implementation for logging.
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
+
+    // Usually don't use this in an async function...
+    // Look up Resource Acquisition Is Initialization (RAII)
+    let _request_span_guard = request_span.enter();
+
     // `r#`: Rust String Literals: https://doc.rust-lang.org/reference/tokens.html#raw-string-literals
     match sqlx::query!(
         r#"
@@ -35,12 +42,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            tracing::info!("{}New subscriber details have been saved", request_id_log);
+            tracing::info!("New subscriber details have been saved");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            tracing::error!("{}Failed to execute query: {:?}", request_id_log, e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
+
+    // Once done, the trace span will exit()
+    // You can enter/exit multiple times, but close only once.
 }
