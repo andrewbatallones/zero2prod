612f307d35f7b7e1ad6ed0ed28c3627d1ff11aca
Andrew Batallones <batallonesa@gmail.com>
2024-05-08 10:07:36 -0400

    refactor tracing to an fn attribute

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,24 +11,19 @@ pub struct FormData {
 }
 
 /// POST /subscribe
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-
-    // Span also have their own levels
-    // This creates a span at the info-level
-    // `%` using that symbol is telling tracing to use their Display implementation for logging.
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
+// Instead of manually writing spans, we can just add an attribute to the fn to trace the function
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        // `%` using that symbol is telling tracing to use their Display implementation for logging.
+        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
-    );
-
-    // Usually don't use this in an async function...
-    // Look up Resource Acquisition Is Initialization (RAII)
-    let _request_span_guard = request_span.enter();
-
-    // Don't need to call .enter() on this query_span, .instrument will take caure of it
+        )
+    )]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    // Don't need to call .enter() on this query_span, instrument will take caure of it
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
     // `r#`: Rust String Literals: https://doc.rust-lang.org/reference/tokens.html#raw-string-literals
     match sqlx::query!(
@@ -45,10 +40,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!("New subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
